0f1fdd3a2b1cc5149c5061218c153e54d901b48e
Oorange2 <220792591+Oorange2@users.noreply.github.com>
2026-06-08 21:44:51 -0400

    feat: add billboard auto-update startup script

---
+++ billboard_startup.lua
@@ -0,0 +1,22 @@
+-- Billboard auto-updater & launcher
+local URL  = "https://raw.githubusercontent.com/Oorange2/cc-player-radar/main/billboard.lua"
+local FILE = "billboard.lua"
+
+local function readLocal()
+    if not fs.exists(FILE) then return nil end
+    local f = fs.open(FILE, "r")
+    local c = f.readAll() f.close() return c
+end
+
+local ok, res = pcall(http.get, URL)
+if ok and res then
+    local remote = res.readAll() res.close()
+    if remote ~= readLocal() then
+        local f = fs.open(FILE, "w") f.write(remote) f.close()
+        print("Billboard updated! Rebooting...") sleep(1) os.reboot()
+    end
+else
+    print("No internet, using local copy")
+end
+
+shell.run(FILE)
